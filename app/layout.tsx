32dc791c204cf56da226625afa985df3baf2a314
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-07-21 20:43:29 -0300

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, Space_Grotesk  } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 
+import { Analytics } from '@vercel/analytics/react';
+
 const spaceGrotesk = Space_Grotesk({
   weight: ['300','400', '700'],
   style: ['normal'],
@@ -25,6 +27,7 @@ export default function RootLayout({
       <body className={spaceGrotesk.className}>
       <Navbar></Navbar>
         {children}
+        <Analytics />
         </body>
     </html>
   );
