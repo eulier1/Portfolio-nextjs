2f00fe1fdf449234bde66c450e5f6a4693f5a5eb
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-07-25 18:27:44 -0300

    fix images for cases section

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,23 +9,28 @@ export type CardType = {
 
 const Card = ({title, description, images, imageDemo}: CardType) => {
     return (
-        <div style={{"height": "560px"}} 
-        className="p-4 md:grid-template-row-1 md:grid-temaple-cols-1 md:grid md:grid-template-rows-4 md:gap-12 text-center bg-neutral-1000 md:p-8 rounded-3xl relative overflow-hidden" >
-            <h2 >{title}</h2>
+        <div style={{"height": "480px"}} 
+        className="p-4 text-center bg-neutral-1000 md:p-8 rounded-3xl relative overflow-hidden" >
+            <h2 className="mb-10" >{title}</h2>
             {description ? <p>{description}</p> : null}  
-            <div  className="flex justify-center mb-16">
+            <div  className="flex justify-center mb-4">
             { images ? images.map( (image, id) => 
             (
                 <div key={id} className="p-4 mx-2 md:p-4 bg-slate-100 rounded-3xl md:mx-4 h-fit -mt-15">
-                    <Image  className="" width={70} height={70} alt="React" src={image.url}/>
+
+                        <Image  className="" width={70} height={70} alt={image.url} src={image.url}/>
+
                 </div>
             )) : null }
             </div>
-            {/* { imageDemo ? (
-                <div className="absolute flex justify-center -bottom-60 left-0 right-0">
-                    <Image className="relative w-1/2" width={80} height={250} alt="React" src={imageDemo}/>
+            { imageDemo ? (
+                
+                <div className="absolute flex justify-center -bottom-24 left-0 right-0 ">
+                    <div className="relative p-2 md:p-4 bg-neutral-500 rounded-lg">
+                        <Image className="relative object-none w-auto"  alt="demo image" src={imageDemo}/>
+                    </div>
                 </div>
-            ): null} */}
+            ): null}
         </div>
     )
 }
